790279c9d01e225a74b10c5a7f1124f0148202ab
Navodya De Silva <125857483+NavodyaD@users.noreply.github.com>
2024-04-14 13:27:35 +0530

    Structure of Band Details Page
    Structure of band details page

---
+++ banddetailspg.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+</head>
+<body>
+    <section id="header">
+        <a href="#"> <img src="assets/img/logo_image.png" class="logo" alt="" width="100" ></a>
+
+        <div>
+            <ul id="navbar">
+                <li><a class="active" href="#">Events</a></li>
+                <li><a href="#">Bands</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="nav-right-set">
+            <a href="" class="icon"><i class="fa fa-user" aria-hidden="true"></i></a>
+            <a href="">My Profile</a>
+        </div>
+    </section>
+
+    <section id="band-details">
+        <div class="main-container">
+            <div class="image-container">
+                <img src="assets/img/band_1.jpg" alt="">
+            </div>
+            <div class="details-container">
+                <h3>Wayo</h3>
+                <p>Description Here</p>
+                <span class="details">
+                    <i class="fa fa-music" aria-hidden="true"></i>
+                    <h5>Text</h5>
+                </span>
+                <span class="details">
+                    <i class="fa fa-music" aria-hidden="true"></i>
+                    <h5>Text </h5>
+                </span>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="newsletter" class="section-p1 section-m1">
+        <div class="newstwxt">
+            <h4>Subscribe for News Letter</h4>
+            <p>Get E-mail about our special <span>announcements</span></p>
+        </div>
+        <div class="form">
+            <input type="text" placeholder="Your Email Address">
+            <button>Subscrive</button>
+        </div>
+    </section>
+
+    <footer class="section-p1">
+        <div class="col">
+            <div class="contact">
+                <img class="logo" src="assets/img/logo_image.png" width="140px" alt="">
+                <h4>Contact</h4>
+                <p><strong>Email:</strong> contact@tickets.lk</p>
+                <p><strong>Phone:</strong> 076 6929 822</p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="social">
+                <h4>Connect with us</h4>
+                <div class="icon">
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-youtube"></i>
+                </div>
+            </div>
+            <h4>Pages</h4>
+            <a href="#">Events</a>
+            <a href="#">How it works</a>
+            <a href="#">About Us</a>
+            <h4>Help</h4>
+            <a href="#">Customer Support</a>
+        </div>
+        <div class="col">
+            <div class="payment">
+                <h4>Payment Gateways</h4>
+                <p>Secure Playment Gateways</p>
+                <div class="row">
+                    <img src="assets/img/visa_logo.jpg" height="40px" alt="">
+                    <img src="assets/img/mastercard_logo.png" height="40px" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="colinstall">
+            <div class="install">
+                <h4>Install App</h4>
+            <p>From Google Play or Apple App Store</p>
+            <div class="row">
+                <img src="assets/img/google_play.jpg" height="50px" alt="">
+                <img src="assets/img/app_store.png" height="50px" alt="">
+            </div>
+            </div>
+            
+        </div>
+        
+    </footer>
+</body>
+</html>
